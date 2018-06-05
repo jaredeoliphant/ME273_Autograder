43e746448df51d7031fa064839c15e0b94abb0db
cgrooves <caleb.j.groves@gmail.com>
2018-06-04 22:01:46 -0600

    First commit

---
+++ autograder_core/lab_grader.m
@@ -0,0 +1,31 @@
+function lab_grader(partTables)
+%============================================BEGIN-HEADER=====
+% FILE: lab_grader.m
+% AUTHOR: Caleb Groves
+% DATE: 4 June 2018
+%
+% PURPOSE:
+%   
+%
+% INPUTS:
+%   
+%
+%
+% OUTPUTS:
+%   
+%
+%
+% NOTES:
+%   
+%
+%
+% VERSION HISTORY TRACKED WITH GIT
+%
+%==============================================END-HEADER======
+
+% get number of lab parts coming in
+n = length(partTables);
+
+
+
+end % end of function
