abf28180c2a858704e7dcdbbdb9fb1a753911f75
cgrooves <caleb.j.groves@gmail.com>
2018-08-11 11:03:19 -0600

    Separate function for grading logic

---
+++ autograder_core/gradingLogic.m
@@ -0,0 +1,25 @@
+%============================================BEGIN-HEADER=====
+% FILE: gradingLogic.m
+% AUTHOR: Caleb Groves
+% DATE: 4 August 2018
+%
+% PURPOSE:
+%   This function embodies the logic to determine whether a particular file
+%   is going to be graded, regraded, or whether old scores will be copied
+%   over.
+%
+% INPUTS:
+%   
+%
+%
+% OUTPUTS:
+%   gradingFlag
+%
+%
+% NOTES:
+%   
+%
+%
+% VERSION HISTORY TRACKED WITH GIT
+%
+%==============================================END-HEADER======
